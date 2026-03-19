ec5d0e733fc2c95295357a23ba431768c79bcb99
jonty1231 <jontythakur68@gmail.com>
2026-03-19 13:21:12 +0530

    Add

--- controllers/newsletterController.js
+++ controllers/newsletterController.js
@@ -40,8 +40,51 @@ const newsletter = await Newsletter.create({ email });
 }
 
 
+const getNewsLetter = async(req,res)=>{
+    try {
+        const data = await Newsletter.find().sort({ createdAt: -1 });
+
+    return res.status(200).json({
+      success: true,
+      count: data.length,
+      data,
+    });
+        
+    } catch (error) {
+         console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+    }
+}
+
+const deleteEmail= async(req,res)=>{
+try {
+    const {id} = req.params;
+       const deleted = await Newsletter.findByIdAndDelete(id);
+          if (!deleted) {
+      return res.status(404).json({
+        success: false,
+        message: "Email not found",
+      });
+    }
+      return res.status(200).json({
+      success: true,
+      message: "Deleted successfully",
+   
+    });
+} catch (error) {
+     return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+}
+}
 
 
 module.exports ={
-   createNewsletter 
+   createNewsletter,
+   getNewsLetter ,
+   deleteEmail
 }
--- routes/newsLetterRoute.js
+++ routes/newsLetterRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { createNewsletter } = require("../controllers/newsletterController");
+const { createNewsletter, getNewsLetter, deleteEmail } = require("../controllers/newsletterController");
+const adminAuth = require("../middleware/adminAuth");
 
 const router = express.Router();
 
 router.post("/send",createNewsletter)
-
+router.get("/get",adminAuth,getNewsLetter)
+router.delete("/delete/:id",adminAuth,deleteEmail)
 module.exports = router;
